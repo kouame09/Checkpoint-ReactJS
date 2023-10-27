a7d9db9f3e3712b9ecfaac1d50b593e433f1223d
kouame09 <princekouame7@gmail.com>
2023-10-27 11:29:41 +0000

    commit react app 1

--- src/app.js
+++ src/app.js
@@ -1,34 +1,59 @@
-//Créer un composant app et à l'interieur du composant app, créer un fragment react et à l'interieur créer un div avc le nom de la classe"app". Et à l'interieur du div, utilise react-bootstrap pour créer une barre de navigation, un entete et trois cartes
 import React from 'react';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, Row, Col} from 'react-bootstrap';
+import { Container, Navbar, Card, Row, Col } from 'react-bootstrap';
+import NavBar from './navbar'
 import './App.css';
 
 function App() {
-    return (
-        <div className="app">
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Languages" id="basic-nav-dropdown"></NavDropdown>
-                    </Nav>
-                    <Form inline>
-                        <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
-                </Container>
-            </Navbar>
-            <Container>
-                <Row>
-                    <Col>
-                        <h1>Hello</h1>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className="App">
+        
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand>Mon Application</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            { <NavBar /> /*importation du composant navBar */}
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <h1>Mon En-tête</h1>
+      </Container>
+
+      <Container>
+        <Row>
+          <Col md={4}>
+            <Card>
+              <Card.Img variant="top" src="image1.jpg" />
+              <Card.Body>
+                <Card.Title>Carte 1</Card.Title>
+                <Card.Text>Description de la carte 1.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card>
+              <Card.Img variant="top" src="image2.jpg" />
+              <Card.Body>
+                <Card.Title>Carte 2</Card.Title>
+                <Card.Text>Description de la carte 2.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card>
+              <Card.Img variant="top" src="image3.jpg" />
+              <Card.Body>
+                <Card.Title>Carte 3</Card.Title>
+                <Card.Text>Description de la carte 3.</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
